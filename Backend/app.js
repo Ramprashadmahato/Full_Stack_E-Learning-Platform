634b646064a0c8007db36cf8b8046a4e48360aec
Ram Prashad Mahato <rpxingh201@gmail.com>
2026-05-19 19:23:55 +0545

    Fixed CORS for deployed frontend

--- Backend/app.js
+++ Backend/app.js
@@ -27,7 +27,10 @@ const server = http.createServer(app);
 // ======================
 const io = new Server(server, {
   cors: {
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://full-stack-e-learning-platform-opal.vercel.app",
+    ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   },
